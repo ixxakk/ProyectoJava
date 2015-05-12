5e55f65336f1492d5208b8c1ab8b8959ddcabf0b
isaac <isaac@isaac-S551LB>
2015-05-12 12:10:43 -0600

    subiendo el proyecto

---
+++ EjerciciosJava/src/ejercicio1/ejercicio1.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio1;
+
+/**
+ *
+ * @author isaac
+ */
+public class ejercicio1 {
+    public static void main(String[]args){
+        System.out.println("numeros del 1 al 100");
+        for (int i = 0; i <= 100; i++) {
+            System.out.println(i);
+        }
+    }
+    
+}
