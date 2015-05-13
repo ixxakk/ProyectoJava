c097c8d8b3ecf1d91180423290ede9323c4c9cde
isaac <isaac@isaac-S551LB>
2015-05-13 17:07:02 -0600

    subiendo el proyecto

--- EjerciciosJava/src/ejercicio2/ejercicio2.java
+++ EjerciciosJava/src/ejercicio2/ejercicio2.java
@@ -3,16 +3,16 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ejercicio1;
+package ejercicio2;
 
 /**
  *
  * @author isaac
  */
-public class ejercicio1 {
+public class ejercicio2 {
     public static void main(String[]args){
-        System.out.println("numeros del 1 al 100");
-        for (int i = 1; i <= 100; i++) {
+        System.out.println("numeros del 100 al 0 decrecientemente");
+        for (int i = 100; i >= 0; i--) {
             System.out.println(i);
         }
     }
---
+++ EjerciciosJava/src/ejercicio3/ejercicio3.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio3;
+
+/**
+ *
+ * @author isaac
+ */
+public class ejercicio3 {
+    public static void main(String[] args) {
+      int numero=1;
+      int limite=100;
+    while(numero <= 100){
+      if(numero%2==0){
+System.out.println(numero);
+}
+numero++;
+ }
+}
+}
